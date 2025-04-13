173b2101924330224b870a52bf1ce77c1dc455ed
samsolt <samsolt1273@gmail.com>
2025-04-13 21:20:03 +0200

    update main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+    int volba;
+
+    std::cout << "CPPRPG" << std::endl;
+    std::cout << "(1) Start" << std::endl;
+    std::cout << "(2) Exit" << std::endl;
+    std::cin >> volba;
+
+    switch(volba){
+        case 1:
+            //placeholder, tady urcite jeste neco bude
+            break;
+        case 2:
+            return 0;
+        default:
+            return 0;
+    }
+}
