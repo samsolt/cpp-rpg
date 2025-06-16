06e424a1969ad20a7968f9fc3f7d90b203d2c93c
samsolt <samsolt1273@gmail.com>
2025-06-16 18:06:00 +0200

    add basic output

--- main.cpp
+++ main.cpp
@@ -1,30 +1,41 @@
-#include "utility.cpp"
 #include <iostream>
+#include <pthread.h>
 #include <string>
-#include <unistd.h> // na sleep
+#include <cstdlib>
+using namespace std;
 
-int main() {
-  int choice;
-  std::string weaponInv[2] = {"empty", "empty"};
+int maxHealth = 100;
+int health = maxHealth;
+int maxStamina = 100;
+int stamina = maxStamina;
+int maxMana = 100;
+int mana = maxMana;
+int gold = 0; // balance uzivatele penizek
+int defense = 1; // hodnota kterou se vynasobi prijaty dmg, eg. health -= health-dmg*defense
+float attack = 1; // ditto ale s damagem rozdanym
 
-  while (true) {
-    std::cout << "CPPRPG" << std::endl;
-    std::cout << "(1) Start" << std::endl;
-    std::cout << "(2) Exit" << std::endl;
-    std::cin >> choice;
+bool inBattle = false;
+bool inVillage = false;
 
-    switch (choice) {
-    case 1:
-      clear();
-      break;
-    case 2:
-      clear();
-      return 0;
-    default:
-      clear();
-      std::cout << "invalid input!\n\n";
-      continue;
-    }
-    break;
+void refresh(){ // aktualizuje zobrazene informace
+  system("clear");
+  cout << "Health\tStamina\tMana\tGold\n";
+  cout << health << "/" << maxHealth << "\t" << stamina << "/" << maxStamina << "\t" << mana << "/" << maxMana << endl;
+  cout << "-----------------------------\n";
+
+  if(inVillage == true){
+    cout << "Nacházíte se ve vesnici\n\n";
+    cout << "1) Bylinkářka\t2) Kovář\t3) Vyprovokovat stráže\t4) Fontána fentanylu\t5) Vydat se do lesa (pokračovat)\n";
   }
+  if(inBattle == true){
+    cout << "Jste v bitvě!\n\n";
+    cout << "1) Útok\t2) Utéct\n";
+  }
+}
+
+int main()
+{
+  char input;
+
+  refresh();
 }
